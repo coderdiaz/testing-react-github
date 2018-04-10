ba7da99d99ba6cd4e360966b4d1ace8a6fed6397
Javier Diaz <coderdiaz@gmail.com>
2018-04-10 00:40:41 -0500

    Config for Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import {ApolloProvider} from 'react-apollo'
+import {ApolloClient} from 'apollo-client'
+import {HttpLink} from 'apollo-link-http'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+
+// Added root service for Github Graph QL API V4
+const httpLink = new HttpLink({uri: 'https://api.github.com/graphql'})
+
+// Configuring Apollo Client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 /**
  * Entry point into the application.
@@ -9,5 +23,10 @@ import registerServiceWorker from './registerServiceWorker'
  * if you don't want to use it, just comment
  * registerServiceWorker();
  */
-ReactDOM.render(<div/>, document.getElementById('root'))
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>, 
+  document.getElementById('root')
+)
 registerServiceWorker()
