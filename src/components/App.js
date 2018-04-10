30915bec26e82cf1d90915faaf080503de5dadf9
Javier Diaz <coderdiaz@gmail.com>
2018-04-10 00:40:31 -0500

    Added App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="application">
+        <h1>Hello React</h1>
+      </div>
+    )
+  }
+}
+
+export default App
