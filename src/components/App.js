8d44e5173d9eeb727dc7a048383c5d0b82532536
Javier Diaz <coderdiaz@gmail.com>
2018-04-10 22:44:12 -0500

    Minor funcionality for App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react'
+import IssueList from './IssueList'
+import Header from './Header'
+import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className="application">
-        <h1>Hello React</h1>
+        <Header/>
+        <main className="main">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-three-fifths is-offset-one-fifth">
+                <IssueList/>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import registerServiceWorker from './registerServiceWorker'
 import {ApolloProvider} from 'react-apollo'
 import {ApolloClient} from 'apollo-client'
 import {HttpLink} from 'apollo-link-http'
+import {setContext} from 'apollo-link-context' 
 import {InMemoryCache} from 'apollo-cache-inmemory'
 
 // Added root service for Github Graph QL API V4
 const httpLink = new HttpLink({uri: 'https://api.github.com/graphql'})
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`
+    }
+  }
+})
 
 // Configuring Apollo Client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
